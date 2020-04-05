8f1e1edc6c74c536f5142ce45e55a1beab078fd8
Roman V. Prikhodchenko <chujoii@gmail.com>
2020-04-06 02:09:36 +0300

    Generate JSON for test (100000 elements)

---
+++ src/client/generate.json.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+echo please wait ...
+
+rm -f test_parser.json test_render.json
+
+# for old bash:
+#for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+
+date +'%F %H:%M:%S'
+echo generate test_parser.json
+# for bash version 3.0+
+for i in {1..100}
+do
+    cat example.json >> test_parser_100.json
+done
+
+for i in {1..1000}
+do
+    cat test_parser_100.json >> test_parser.json
+done
+rm -f test_parser_100.json
+
+date +'%F %H:%M:%S'
+echo generate test_render.json
+for i in {1..100000}
+do
+    echo \"user\": \"`pwgen -A -0 -1`\" >> test_render.json
+done
+date +'%F %H:%M:%S'
