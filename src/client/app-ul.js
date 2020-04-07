359971a9e190b055de26a0de15f1bd70583508f8
Roman V. Prikhodchenko <chujoii@gmail.com>
2020-04-07 19:43:58 +0300

    Add fake scroll bar

--- src/client/app-ul.js
+++ src/client/app-ul.js
@@ -1,16 +1,32 @@
 "use strict";
 
+const max_scroll_bar_position = 60;
+const min_scroll_bar_position = 20;
+
 var appul = new Vue({
 	el: '#app-ul',
 	data: {
-		filename: '',
+		file_name: '',
 		seen_dz: true,
 		seen_ul: false,
-		db: []
+		db: [],
+		scroll_bar_position: 40,
+		scroll_bar_step:1
 	},
 	methods: {
 		delete_user: function (index) {
 			this.db.splice(index, 1);
+		},
+		scroll_up: function () {
+			this.scroll_bar_position -= (min_scroll_bar_position < this.scroll_bar_position) ? this.scroll_bar_step : 0;
+			console.log ("up " + this.scroll_bar_position);
+		},
+		scroll_down: function () {
+			this.scroll_bar_position += (max_scroll_bar_position > this.scroll_bar_position) ? this.scroll_bar_step : 0;
+			console.log ("down " + this.scroll_bar_position);
+		},
+		scroll_bar: function () {
+			console.log("scroll_bar");
 		}
 	}
 })
